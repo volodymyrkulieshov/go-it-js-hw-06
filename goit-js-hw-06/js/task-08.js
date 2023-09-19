fd794ac0825b9beb623ba9e04309f2faf168ddc1
Volodymyr <vovkul@gmail.com>
2023-09-19 23:50:10 +0200

    task 8

--- goit-js-hw-06/js/task-08.js
+++ goit-js-hw-06/js/task-08.js
@@ -1,1 +1,16 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  const FormElem = evt.currentTarget.elements;
+  const email = FormElem.email.value;
+  const password = FormElem.password.value;
+  if (email === "" || password === "") {
+    alert("Введіть дані");
+    return;
+  } else {
+    return loginForm.reset();
+  }
+}
