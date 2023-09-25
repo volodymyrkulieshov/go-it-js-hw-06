a33a909b21bf211e370731137edc7791e9de25b0
Volodymyr <vovkul@gmail.com>
2023-09-25 10:47:30 +0200

    Update task-09.js

--- goit-js-hw-06/js/task-09.js
+++ goit-js-hw-06/js/task-09.js
@@ -3,3 +3,22 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+// const buttonChangeEl = document.querySelector(".change-color");
+// const spanEl = document.querySelector(".color");
+
+// buttonChangeEl.addEventListener("click", () => {
+//   const bkgColor = (document.body.style.backgroundColor = getRandomHexColor());
+//   spanEl.innerHTML = bkgColor;
+// });
+
+const btnChangeEl = document.querySelector(".change-color");
+const backgroundColorEL = document.querySelector(".color");
+
+btnChangeEl.addEventListener("click", onClick);
+
+function onClick() {
+  const changeBgColor = (document.body.style.backgroundColor =
+    getRandomHexColor());
+  backgroundColorEL.innerHTML = changeBgColor;
+}
